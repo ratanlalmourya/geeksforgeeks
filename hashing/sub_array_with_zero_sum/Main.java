256c82d7c20a0d1e181f1e8ad147488ebff719d4
Ratan Mourya <68645660+ratanlalmourya@users.noreply.github.com>
2024-05-30 02:23:45 +0000

    Efficient Solution of subarray zero

--- hashing/sub_array_with_zero_sum/Main.java
+++ hashing/sub_array_with_zero_sum/Main.java
@@ -1,12 +1,50 @@
 package hashing.sub_array_with_zero_sum;
 
+import java.util.HashSet;
+
 public class Main {
     
     public static void main(String[] args) {
         
         Integer[] array = { 1, 4, 13, -3, -10, 5};
-        find_zero_sum_array_n2(array);
 
+        // Naive solution
+        long start = System.nanoTime();
+        find_zero_sum_array_n2(array);
+        long finish = System.nanoTime();
+        System.out.println(finish - start);
+        // Efficient Solution
+        System.out.println("*************************");
+        start = System.nanoTime();
+        find_zero_sum_array_n1(array);
+        finish = System.nanoTime();
+        System.out.println(finish - start);
+
+    }
+
+    public static void find_zero_sum_array_n1(Integer[] array) {
+
+        HashSet<Integer> hashSet = new HashSet<>();
+
+        Integer prevSum = 0;
+        for (int i = 0; i < array.length; i++) {
+
+            prevSum += array[i];
+
+            if (hashSet.contains(prevSum)) {
+                System.out.println("YES");
+                return;
+            }
+
+            if (prevSum == 0) {
+                System.out.println("YES");
+                return;
+            }
+
+            hashSet.add(prevSum);
+        }
+
+        System.out.println("NO");
     }
 
     public static void find_zero_sum_array_n2(Integer[] array) {
