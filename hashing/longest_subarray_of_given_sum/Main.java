cf570c63fe46488d4c36035f74f57e7e5f728189
Ratan Mourya <68645660+ratanlalmourya@users.noreply.github.com>
2024-06-01 15:21:07 +0000

    longest sub array of given sum with O(n) complexity

--- hashing/longest_subarray_of_given_sum/Main.java
+++ hashing/longest_subarray_of_given_sum/Main.java
@@ -1,5 +1,7 @@
 package hashing.longest_subarray_of_given_sum;
 
+import java.util.HashMap;
+
 public class Main {
     
     public static void main(String[] args) {
@@ -11,12 +13,23 @@ public class Main {
         Integer[] array3 = {8, 3, 7};
         Integer sum3= 15;
 
+        // Naive solution
         Integer sub_length = find_longest_subarray_of_given_sum_n2(array1,sum1);
         System.out.println(sub_length);
         sub_length = find_longest_subarray_of_given_sum_n2(array2,sum2);
         System.out.println(sub_length);
         sub_length = find_longest_subarray_of_given_sum_n2(array3,sum3);
         System.out.println(sub_length);
+        System.out.println("******************************");
+
+        // Efficient solution
+        sub_length = find_longest_subarray_of_given_sum_n1(array1,sum1);
+        System.out.println(sub_length);
+        sub_length = find_longest_subarray_of_given_sum_n1(array2,sum2);
+        System.out.println(sub_length);
+        sub_length = find_longest_subarray_of_given_sum_n1(array3,sum3);
+        System.out.println(sub_length);
+
     }
 
     public static Integer find_longest_subarray_of_given_sum_n2(Integer[] array,Integer sum) {
@@ -32,5 +45,28 @@ public class Main {
         }
 
         return longest_subsequence;
+    }
+
+    public static Integer find_longest_subarray_of_given_sum_n1(Integer[] array,Integer sum) {  
+        Integer result = 0;
+        HashMap<Integer,Integer> hashMap = new HashMap<>();
+        Integer prevSum = 0;
+        for (int i = 0; i < array.length; i++) {
+            prevSum += array[i];
+
+            if(!hashMap.containsKey(prevSum)) {
+                 hashMap.put(prevSum, i);
+            }
+
+            if(hashMap.containsKey(prevSum - sum))
+            {
+                result =  Math.max(result, i - hashMap.get(prevSum - sum));
+            }
+
+            if(prevSum == sum) {
+                result =  Math.max(result, i + 1); 
+            }
+        }
+        return result;
     }
 }
