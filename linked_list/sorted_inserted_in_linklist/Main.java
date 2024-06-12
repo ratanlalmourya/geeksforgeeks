e36f4893214c4d7b8b29dbfa517f105583e72c6d
ratanlalmourya <ratanlalmourya41@gmail.com>
2024-06-12 22:43:34 +0530

    inserted new element in sorted linked list

---
+++ linked_list/sorted_inserted_in_linklist/Main.java
@@ -0,0 +1,52 @@
+package linked_list.sorted_inserted_in_linklist;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        Node head = new Node(10);
+        Node node2 = new Node(20);
+        Node node3 = new Node(30);
+        Node node4 = new Node(40);
+ 
+        // link nodes
+        head.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+
+        head = insert_node_in_sorted_linked_list(head,5);
+        linked_list_recursive(head);
+
+    }
+
+    public static Node insert_node_in_sorted_linked_list(Node head,int val) { 
+
+        Node newNode = new Node(val);
+        Node temp = head;
+
+        if(temp.data > val) {
+            newNode.next = temp;
+            return newNode;
+        }
+
+        while (temp != null) {
+            if(temp.next == null || temp.next.data > val) {
+                break;
+            }
+            temp = temp.next;
+        }
+        newNode.next = temp.next;
+        temp.next = newNode;
+        return head;
+    }
+
+    public static void linked_list_recursive(Node head) {
+        if(head == null) {
+            return;
+        }
+        System.out.print(head.data + " ") ;
+        linked_list_recursive(head.next);
+    }
+
+
+}
---
+++ linked_list/sorted_inserted_in_linklist/Node.java
@@ -0,0 +1,10 @@
+package linked_list.sorted_inserted_in_linklist;
+
+public class Node {
+    Node next;
+    Integer data;
+    public Node(Integer data) {
+        this.next = null;
+        this.data = data;
+    }
+}
