dfc4aa32c0e28174660035733e4c50cefe1f0eeb
mouryatutor <156491972+mouryatutor@users.noreply.github.com>
2024-07-11 02:48:10 +0000

    Efficient solution of finding prime factors

---
+++ mathematics/prime_factors/Main.java
@@ -0,0 +1,32 @@
+package mathematics.prime_factors;
+
+import java.util.ArrayList;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        ArrayList<Integer> result = find_prime_factors_v1(450);
+
+        //315  105 35 7
+
+        for (Integer primeFactor : result) {
+            System.out.print(primeFactor + " ");
+        }
+        System.out.println();
+    }
+
+    public static ArrayList<Integer> find_prime_factors_v1(Integer n) {
+        ArrayList<Integer> primeFactors = new ArrayList<>();
+        if( n <= 1) {
+            return primeFactors;
+        }
+        for (int i = 2; i * i <= n; i = i + 1) {     
+            while(n > 0 && n % i == 0) {
+                primeFactors.add(i);
+                n = n / i;
+            } 
+        }
+        return primeFactors;
+    }
+}
