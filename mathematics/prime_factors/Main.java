c2b080c336befe50a5fa936cf77f7826522616df
mouryatutor <156491972+mouryatutor@users.noreply.github.com>
2024-07-11 03:06:59 +0000

    Efficient solution of prime factorixation

--- mathematics/prime_factors/Main.java
+++ mathematics/prime_factors/Main.java
@@ -6,14 +6,56 @@ public class Main {
     
     public static void main(String[] args) {
         
-        ArrayList<Integer> result = find_prime_factors_v1(450);
+        ArrayList<Integer> result = find_prime_factors_v1(415);
 
         //315  105 35 7
 
         for (Integer primeFactor : result) {
             System.out.print(primeFactor + " ");
         }
+        System.out.println("**************  Efficient Solution *******************");
+        // 5 7    11 13    17 19     23 25   29 32    35 37   41 43
+        result = find_prime_factors_effcient_solution(415);
+        for (Integer primeFactor : result) {
+            System.out.print(primeFactor + " ");
+        }
         System.out.println();
+    }
+
+    private static ArrayList<Integer> find_prime_factors_effcient_solution(int n) {
+        ArrayList<Integer> primeFactors = new ArrayList<>();
+
+        while (n > 0 && n % 2 == 0) {
+            primeFactors.add(2);
+            n = n / 2;
+        }
+
+        while (n > 0 && n % 3 == 0) {
+            primeFactors.add(3);
+            n = n / 3;
+        }
+
+        for (int i = 5; i * i <= n ; i = i + 6) {
+            
+            while(n % i == 0) {
+                primeFactors.add(i);
+                n = n / i;
+            }
+
+            while(n % (i + 2) == 0) {
+                primeFactors.add(i + 2);
+                n = n / (i + 2);
+            }
+
+        }
+
+        // corner case
+
+        if(n > 1) {
+            primeFactors.add(n);
+        }
+
+        return primeFactors;
     }
 
     public static ArrayList<Integer> find_prime_factors_v1(Integer n) {
