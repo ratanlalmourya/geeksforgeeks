d0488cb500d1cd2a2d2f8d81fcaff43e69769e27
Ratan Mourya <68645660+ratanlalmourya@users.noreply.github.com>
2024-08-25 04:08:08 +0000

    power set using bitwsise operator

---
+++ bit_magic/power_set_using_bitwise/Main.java
@@ -0,0 +1,30 @@
+package bit_magic.power_set_using_bitwise;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        String s = "abc";
+        power_set_using_bitwsise_v1(s);
+    }
+
+    private static void power_set_using_bitwsise_v1(String str) {
+        Double k = (double) str.length();
+        Integer n = (int) Math.pow(2, k);
+        for (int i = 0; i < n; i++) {
+            int currentItem = i;
+            String sp = "";
+            int index = 0;
+            while(currentItem > 0) {
+                if((currentItem & 1) != 0) {
+                    sp = sp + str.charAt(index);
+                }
+                currentItem = currentItem >> 1;
+                index++;
+            }
+
+            System.out.println(sp);
+            
+        }
+    }
+
+}
